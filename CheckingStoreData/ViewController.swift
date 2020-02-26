ab2db9575d1679357fdff7343fb4c9a44cebecd0
Виктор Сафонов <safonov.bogi@yandex.ru>
2020-02-26 11:06:44 +0300

    Initial Commit

---
+++ CheckingStoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckingStoreData
+//
+//  Created by мак on 26.02.2020.
+//  Copyright © 2020 viktorsafonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
